69b09794e89838eac2a1703451eb94793675c401
mreid <mark@reid.name>
2009-03-18 19:17:44 +1100

    Minor refactor

--- test.clj
+++ test.clj
@@ -7,20 +7,23 @@
 (import '(org.biojava.bio.seq DNATools))
 (import '(org.biojavax.bio.seq RichSequence$IOTools))
 
+;; Helper for showing BioJava sequences
+(defn show [sequence] (. sequence seqString))
+
 ;; Read in the DNA file using a Fasta parser
 (def input (BufferedReader. (FileReader. "data/dm4p1/dm4p1.dEID")))
 (def data (RichSequence$IOTools/readFastaDNA input nil))
 
 ;; Grab the first sequence and print it out
-(def dseq (.. data nextRichSequence))
-(prn 'Sequence (. dseq seqString))
+(def dseq (. data nextRichSequence))
+(prn 'Sequence (show dseq))
 
 ;; Set up a pattern "agnct" ("n" = "ambigous"match anything") to find
 (def pattern (MaxMismatchPattern. (DNATools/createDNA "agnct") 0))
 (def matcher (. pattern matcher dseq))
-(prn 'Pattern (.. pattern getPattern seqString))
+(prn 'Pattern (show (. pattern getPattern)))
 
 ;; Get the first match and print it out
 (. matcher find)
 (def match (. matcher group))
-(prn 'Match (. match seqString))
+(prn 'Match (show match))
