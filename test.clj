babc4d311847c34eb4b02ce0bc1158ebc4be6e95
mreid <mark@reid.name>
2009-03-21 15:45:31 +1100

    Tweaks to make Java calls code more idiomatic

--- test.clj
+++ test.clj
@@ -8,22 +8,22 @@
 (import '(org.biojavax.bio.seq RichSequence$IOTools))
 
 ;; Helper for showing BioJava sequences
-(defn show [sequence] (. sequence seqString))
+(defn show [sequence] (.seqString sequence))
 
 ;; Read in the DNA file using a Fasta parser
 (def input (BufferedReader. (FileReader. "data/dm4p1/dm4p1.dEID")))
 (def data (RichSequence$IOTools/readFastaDNA input nil))
 
 ;; Grab the first sequence and print it out
-(def dseq (. data nextRichSequence))
+(def dseq (.nextRichSequence data))
 (prn 'Sequence (show dseq))
 
 ;; Set up a pattern "agnct" ("n" = "ambigous"match anything") to find
 (def pattern (MaxMismatchPattern. (DNATools/createDNA "agnct") 0))
-(def matcher (. pattern matcher dseq))
-(prn 'Pattern (show (. pattern getPattern)))
+(def matcher (.matcher pattern dseq))
+(prn 'Pattern (show (.getPattern pattern)))
 
 ;; Get the first match and print it out
 (. matcher find)
-(def match (. matcher group))
+(def match (.group matcher))
 (prn 'Match (show match))
